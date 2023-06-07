71fefbd70a07056ae88374cfca55bd73fa1b1dbc
root <thamiemathebula@gmail.com>
2023-06-07 16:35:06 -0700

    fixed requirement errors

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,13 +1,30 @@
+#!/usr/bin/env python3
 from flask import request
 from typing import List, TypeVar
 
+"""
+A template for all authentification you will implement
+"""
+
 
 class Auth:
+    """
+    Auth template
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Lists str
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+        Authorization hearder function
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Current user
+        """
         return None
