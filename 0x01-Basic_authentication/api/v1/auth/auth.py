c8802b597f76e7f813361709cc7938c24feb514d
root <thamiemathebula@gmail.com>
2023-06-07 17:02:38 -0700

    resolved documentation error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python3
+
+"""a template fot the user class"""
+
 from flask import request
 from typing import List, TypeVar
 
-"""
-A template for all authentification you will implement
-"""
-
-
 class Auth:
-    """
-    Auth template
-    """
+    """Auth template"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        Lists str
-        """
+        """checksnif a user is authenticated"""
+
         return False
 
     def authorization_header(self, request=None) -> str:
-        """
-        Authorization hearder function
-        """
+        """Authorization hearder function"""
+
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        Current user
-        """
+        """Current user"""
+
         return None
