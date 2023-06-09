85da8d8e217ca7305061404a73607dd14ea69558
NomatheLuckyOne <thamiemathebula@gmail.com>
2023-06-09 01:10:51 +0000

    fixed error on db engine

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -34,7 +34,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """add user to the db"""
 
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        user_info = User(email=email, hashed_password=hashed_password)
+        self._session.add(user_info)
         self._session.commit()
-        return new_user
+        return user_info
