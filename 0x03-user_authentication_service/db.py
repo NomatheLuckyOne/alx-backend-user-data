6814beb7bdc7d78184153d71ce3e6637eea97da2
NomatheLuckyOne <thamiemathebula@gmail.com>
2023-06-13 00:44:14 +0000

    fixed error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -39,7 +40,7 @@ class DB:
         self._session.commit()
         return user_info
 
-    def find_user(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """find user on the db"""
 
         try:
